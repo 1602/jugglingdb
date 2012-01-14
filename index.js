1a71a82b8f18e21b425396bcbc5f943e4febbf31
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-01-14 12:24:24 +0400

    Safe require package.json

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
-exports.version = JSON.parse(require('fs').readFileSync('./package.json')).version;
+try {
+    exports.version = require('./package.json').version;
+} catch(e) {}
