7f359c808eb3b3295dde83a28a8ab53b936fdffd
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-01-26 13:36:14 +0400

    Get version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var fs = require('fs');
+
 exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
 try {
     if (process.versions.node < '0.6' || true) {
-        exports.version = JSON.parse(fs.readFileSync(process.cwd() + '/package.json')).version;
+        exports.version = JSON.parse(fs.readFileSync(__dirname + '/package.json')).version;
     } else {
         exports.version = require('../package').version;
     }
-} catch(e) {}
+} catch (e) {}
