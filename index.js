6ee7de0716d0b6f5f5a537ea549cb55cfbc6fc57
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-03-27 18:22:24 +0400

    Document

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var fs = require('fs');
+var path = require('path');
 
 exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
+exports.init = function () {
+    if (!global.railway) return;
+    railway.orm = exports;
+    require('./lib/railway');
+};
+
 try {
-    if (process.versions.node < '0.6' || true) {
+    if (process.versions.node < '0.6') {
         exports.version = JSON.parse(fs.readFileSync(__dirname + '/package.json')).version;
     } else {
         exports.version = require('../package').version;
