8b4da9e5e9b8c15957f5bc3792574fab323b9964
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-10-29 03:21:51 +0400

    Remove globals, read yaml

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
-exports.init = function (root) {
-    if (!global.railway) return;
-    railway.orm = exports;
-    require('./lib/railway')(root);
+exports.init = function (rw) {
+    if (global.railway) {
+        railway.orm = exports;
+    } else {
+        rw.orm = {Schema: exports.Schema, AbstractClass: exports.AbstractClass};
+    }
+    require('./lib/railway')(rw);
 };
 
 try {
