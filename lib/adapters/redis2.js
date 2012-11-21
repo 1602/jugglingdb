1602/jugglingdb

commit 885e9188f874ea6d5c16d820b7867646f1468f33

fixed type check strictly

lib/adapters/redis2.js
@@ -172,37 +172,98 @@ BridgeToRedis.prototype.defineForeignKey = function (model, key, cb) {
     cb(null, Number);
 };
 
+// BridgeToRedis.prototype.forDb = function (model, data) {
+//     var p = this._models[model].properties;
+//     for (var i in data) {
+//         if (p[i] && p[i].type.name === 'Date') {
+//             data[i] = data[i] && data[i].getTime ? data[i].getTime() : 0;
+//         } else if (p[i] && [
+//             'String', 'Text', 'Number', 'Boolean', 'Date'
+//         ].indexOf(p[i].type.name) === -1) {
+//             data[i] = JSON.stringify(data[i]);
+//         }
+//     }
+//     return data;
+// };
+
 BridgeToRedis.prototype.forDb = function (model, data) {
     var p = this._models[model].properties;
     for (var i in data) {
-        if (p[i] && p[i].type.name === 'Date') {
-            data[i] = data[i] && data[i].getTime ? data[i].getTime() : 0;
-        } else if (p[i] && [
-            'String', 'Text', 'Number', 'Boolean', 'Date'
-        ].indexOf(p[i].type.name) === -1) {
-            data[i] = JSON.stringify(data[i]);
+        if (!p[i]) continue;
+        if (!data[i]) {
+            data[i] = "";
+            continue;
+        }
+        switch (p[i].type.name) {
+            case "Date":
+                data[i] = data[i].getTime ? data[i].getTime().toString() : "0";
+                break;
+            case "Number":
+                data[i] = data[i].toString();
+                break;
+            case "Boolean":
+                data[i] = !!data[i] ? "1" : "0";
+                break;
+            case "String":
+            case "Text":
+                break;
+            default:
+                data[i] = JSON.stringify(data[i]);
         }
     }
     return data;
 };
 
+// BridgeToRedis.prototype.fromDb = function (model, data) {
+//     var p = this._models[model].properties;
+//     for (var i in data) {
+//         if (p[i] && p[i].type.name === 'Date') {
+//             if (isNaN(parseInt(data[i]))) {
+//                 data[i] = new Date(data[i]);
+//             } else {
+//                 var ms = data[i];
+//                 data[i] = new Date();
+//                 data[i].setTime(ms);
+//             }
+//         } else if (p[i] && [
+//             'String', 'Text', 'Number', 'Boolean', 'Date'
+//         ].indexOf(p[i].type.name) === -1) {
+//             try {
+//                 data[i] = JSON.parse(data[i]);
+//             } catch (e) {}
+//         }
+//     }
+//     return data;
+// };
+
 BridgeToRedis.prototype.fromDb = function (model, data) {
-    var p = this._models[model].properties;
+    var p = this._models[model].properties, d;
     for (var i in data) {
-        if (p[i] && p[i].type.name === 'Date') {
-            if (isNaN(parseInt(data[i]))) {
-                data[i] = new Date(data[i]);
-            } else {
-                var ms = data[i];
-                data[i] = new Date();
-                data[i].setTime(ms);
-            }
-        } else if (p[i] && [
-            'String', 'Text', 'Number', 'Boolean', 'Date'
-        ].indexOf(p[i].type.name) === -1) {
-            try {
-                data[i] = JSON.parse(data[i]);
-            } catch (e) {}
+        if (!p[i]) continue;
+        if (!data[i]) {
+            data[i] = "";
+            continue;
+        }
+        switch (p[i].type.name) {
+            case "Date":
+                d = new Date(data[i]);
+                d.setTime(data[i]);
+                data[i] = d;
+                break;
+            case "Number":
+                data[i] = Number(data[i]);
+                break;
+            case "Boolean":
+                data[i] = data[i] === "1";
+                break;
+            default:
+                d = data[i];
+                try {
+                    data[i] = JSON.parse(data[i]);
+                }
+                catch(e) {
+                    data[i] = d;
+                }
         }
     }
     return data;

commit ad1ca5834bcdc5bbe561bcf07d16e643669bb123

Fixed redis2 adapter filtering with Booleans
885e918 caused bug in filtering with Boolean values: it was trying to
find index 'i:Post:published:1', which was not found. Saving indexes in
format that is compatible to previous versions, i.e
'i:Post:published:true'

lib/adapters/redis2.js
@@ -188,7 +188,7 @@ BridgeToRedis.prototype.forDb = function (model, data) {
                 data[i] = data[i].toString();
                 break;
             case "Boolean":
-                data[i] = !!data[i] ? "1" : "0";
+                data[i] = !!data[i] ? "true" : "false";
                 break;
             case "String":
             case "Text":
@@ -218,7 +218,7 @@ BridgeToRedis.prototype.fromDb = function (model, data) {
                 data[i] = Number(data[i]);
                 break;
             case "Boolean":
-                data[i] = data[i] === "1";
+                data[i] = data[i] === "true";
                 break;
             default:
                 d = data[i];

test/common_test.js
@@ -83,7 +83,7 @@ function testOrm(schema) {
             subject:   { type: String },
             content:   { type: Text },
             date:      { type: Date,    default: function () { return new Date }, index: true },
-            published: { type: Boolean, default: false },
+            published: { type: Boolean, default: false, index: true },
             likes:     [],
             related:   [RelatedPost]
         }, {table: 'posts'});
@@ -425,8 +425,8 @@ function testOrm(schema) {
 
     it('should find records filtered with multiple attributes', function (test) {
         Post.create({title: 'title', content: 'content', published: true, date: 1}, function (err, post) {
-            Post.all({where: {title: 'title', date: 1}}, function (err, res) {
-                test.ok(res.length > 0, 'Exact match with string returns dataset');
+            Post.all({where: {title: 'title', date: 1, published: true}}, function (err, res) {
+                test.ok(res.length == 1, 'Filtering Posts returns one post');
                 test.done();
             });
         });