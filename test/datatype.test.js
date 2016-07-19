fc8f0475db90969ffa44c18581cc1b503f789320
Anatoliy Chakkaev <mail@anatoliy.in>
2016-07-19 20:43:25 +0100

    Promisify and fix test

--- test/datatype.test.js
+++ test/datatype.test.js
@@ -11,55 +11,49 @@ describe('datatypes', function() {
             str: String,
             date: Date,
             num: Number,
-            bool: Boolean,
-            list: {type: []},
+            bool: Boolean
         });
         db.automigrate(function() {
             Model.destroyAll(done);
         });
     });
 
-    it('should keep types when get read data from db', function(done) {
+    it('should keep types when get read data from db', function() {
         var d = new Date, id;
 
-        Model.create({
-            str: 'hello', date: d, num: '3', bool: 1, list: ['test']
-        }, function(err, m) {
-            should.not.exist(err);
-            should.exist(m && m.id);
-            m.str.should.be.a.String;
-            m.num.should.be.a.Number;
-            m.bool.should.be.a.Boolean;
-            id = m.id;
-            testFind(testAll);
-        });
+        d.setMilliseconds(0);
 
-        function testFind(next) {
-            Model.find(id, function(err, m) {
-                should.not.exist(err);
+        return Model.create({
+            str: 'hello',
+            date: d,
+            num: '3',
+            bool: 1
+        })
+            .then(m => {
+                should.exist(m && m.id);
+                m.str.should.be.a.String;
+                m.num.should.be.a.Number;
+                m.bool.should.be.a.Boolean;
+                id = m.id;
+                return Model.find(id);
+            })
+            .then(m => {
                 should.exist(m);
                 m.str.should.be.a.String;
                 m.num.should.be.a.Number;
                 m.bool.should.be.a.Boolean;
                 m.date.should.be.an.instanceOf(Date);
                 m.date.toString().should.equal(d.toString(), 'Time must match');
-                next();
-            });
-        }
-
-        function testAll() {
-            Model.findOne(function(err, m) {
-                should.not.exist(err);
+                return Model.findOne();
+            })
+            .then(m => {
                 should.exist(m);
                 m.str.should.be.a.String;
                 m.num.should.be.a.Number;
                 m.bool.should.be.a.Boolean;
                 m.date.should.be.an.instanceOf(Date);
                 m.date.toString().should.equal(d.toString(), 'Time must match');
-                done();
             });
-        }
-
     });
 
     it('should convert "false" to false for boolean', function() {
